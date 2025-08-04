db94d0505cff344029b508d6fe7731fcfd47ad7e
daniel.claessen <daniel.claessen@interlutions.de>
2025-08-04 23:32:24 +0200

    fix: add missing process.exit to avoid hanging on init-db script

--- init-db.ts
+++ init-db.ts
@@ -2,6 +2,9 @@ import { db } from "@exulu/backend"
 
 const init = async () => {
     await db.init()
+     // Important to exit here to avoid hanging the 
+     // process of this script in deployments.
+    process.exit(0)
 }
 
 init()
